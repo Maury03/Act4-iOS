bde52e7795805fa92d2c8ab08ead3b6040d157d9
Maury03 <diegomqh@gmail.com>
2024-02-13 17:15:51 -0600

    Initial Commit

---
+++ Actividad4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividad4
+//
+//  Created by Diego Quintanilla  on 12/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
